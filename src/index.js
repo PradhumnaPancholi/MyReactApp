26fcc467ca86184264d5558114723d21d21d8c4c
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-07-04 18:14:10 -0400

    added videolist component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-import React from 'react'; 
+import React, { Component } from 'react'; 
 import ReactDOM from 'react-dom';
-
+import YTSearch  from 'youtube-api-search';
+import YoutubeAPI from 'youtube-api-client';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const API_KEY = 'AIzaSyBaJ95cDBubLFXn9a8-UJVIdT2FTIBzn7w';
-//create a new component, this component should generate some html//
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+/*const API_KEY = 'AIzaSyBaJ95cDBubLFXn9a8-UJVIdT2FTIBzn7w';
+
+YTSearch = ({key: API_KEY, term: 'Avengers'}, function(data){
+    
+});*/
+const youtube = new YoutubeAPI({
+  apiKey: 'AIzaSyBaJ95cDBubLFXn9a8-UJVIdT2FTIBzn7w'
+});
+ 
+
+class  App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: []};
+
+        youtube.getSearchResultsFor('avengers')
+            .then(videos => {
+                this.setState({ videos });
+                console.log(videos);
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos = {this.state.videos}/>
+            </div>
+        );
+    }
 }
 
-//take this component's generated html and shove it up in the DOM//
+
 ReactDOM.render(<App />, document.querySelector('.container '));
