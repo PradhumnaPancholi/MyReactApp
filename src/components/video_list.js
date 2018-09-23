b798eb75180586d9aa399f75d9340d4eac642d58
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-09-22 20:01:59 -0400

    bundle.js

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,17 +4,17 @@ import VideoListItem from './video_list_item';
 const VideoList = (props) =>{
     const videoItems = props.videos.map((video) => {
         return(
-            <VideoListItem 
+            <VideoListItem
             onVideoSelect = {props.onVideoSelect}
-            key = {video.etag} 
+            key = {video.etag}
             video = { video } />
-        ); 
+        );
     });
     return(
-        <ul className = 'col-md-4 list-group'>
+        <ul className = 'col-md-4 list-group' >
             {videoItems}
         </ul>
     );
 };
 
-export default VideoList; 
+export default VideoList;
